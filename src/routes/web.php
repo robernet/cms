91676e31be3a054bc3f400bb5231c3a7ff72fdce
Enas <enasasmar882@gmail.com>
2022-12-03 01:31:26 +0200

    fix

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Corals\Modules\CMS\Models\Content;
+
 Route::feeds();
 
 Route::group(['prefix' => 'cms', 'as' => 'cms.'], function () {
@@ -28,8 +30,9 @@ Route::group(['prefix' => 'cms', 'as' => 'cms.'], function () {
 
 Route::group(['prefix' => ''], function () {
     Route::get('/', 'FrontendController@index')->name('frontend_home');
-  //  Route::get('{slug}', 'FrontendController@show')->name('frontend_single');
-    Route::get('category/{slug}', 'FrontendController@category')->name('frontend_category');;
+    Route::get('{slug}', 'FrontendController@show')->name('frontend_single')
+        ->where('slug',implode('|',Content::query()->published()->pluck('slug')->toArray()));
+    Route::get('category/{slug}', 'FrontendController@category')->name('frontend_category');
     Route::get('tag/{slug}', 'FrontendController@tag')->name('frontend_tag');
     Route::post('contact/email', 'FrontendController@contactEmail');
     Route::get('admin-preview/{slug}', 'FrontendController@adminShow')->middleware('auth');
